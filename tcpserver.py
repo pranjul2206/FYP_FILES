0e1366c6abe2e81a186c3db4ec36bd0fdae56a55
pranjul2206 <pranjulmishra10112206@gmail.com>
2020-05-06 01:28:07 +0530

    completed connection of volume button to the model

--- tcpserver.py
+++ tcpserver.py
@@ -3,6 +3,8 @@ import numpy
 import os
 import socket
 import win32com.client
+import pickle
+import statistics
 
 
 #root_path
@@ -13,27 +15,27 @@ gesture_dict={'left':os.path.join(dir, 'ANDROID','gestures','left'),
         "top":os.path.join(dir, 'ANDROID','gestures','top'),
         "bottom":os.path.join(dir, 'ANDROID','gestures','bottom')}
 
-def predict():
-    dirname = os.path.join(dir, 'ANDROID','models') #getting path name for model
-    list = os.listdir(dirname) #refrence of elemt is path
-    print(list)
-    max=0 #obtaining max amongt complete list[latest=max]
-    #choosing most recent model,In future user can chossse their own model with their accuracy
-    for i in list:
-        if int(i[12:])>max:
-            max=int(i[12:])
-    import pickle
-    with open(dirname+'/model_pickle'+str(max),'rb') as f: #loading most recent model
+def predict(data_predict):
+    print("in predict")
+    print(data_predict)
+    with open("SVM_pickle",'rb') as f: #loading most recent model
         svc=pickle.load(f)
-    xyz=finaldata.split()
-    print(xyz)
-    df=pd.DataFrame(columns=["x","y","z"])
-    b={"x":xyz[0],"y":xyz[1],"z":xyz[2]} #testing with my own variances
-    df=df.append(b,ignore_index=True)
-    pred=svc.predict(df)
+
+    dfm=pd.DataFrame(columns=["x","y","z"])
+    df_ans=pd.DataFrame(columns=["x","y","z"])
+    for i in data_predict:
+        arr=i.split()
+        b={"x":float(arr[0]),"y":float(arr[1]),"z":float(arr[2])}
+        dfm=dfm.append(b,ignore_index=True)
+    df_ans=df_ans.append({"x":(statistics.pstdev(dfm["x"])),
+        "y":(statistics.pstdev(dfm["y"])),
+        "z":(statistics.pstdev(dfm["z"])),},ignore_index=True)
+    print(df_ans)
+    pred=svc.predict(df_ans)
+    print(pred)
     controll=pred.astype(numpy.int64)
     print("predictions=",controll[0]) #prediction gesture1
-    if controll[0]==0:
+    if controll[0]==3:
         presentation.SlideShowWindow.View.Next()
     elif controll[0]==1:
         presentation.SlideShowWindow.View.Previous()
@@ -51,9 +53,10 @@ def CaptureData(gesture,datas):
     print("gesture",gesture,"\ndatas",datas)
     
     
-
 PORT = 7800
-
+app = win32com.client.Dispatch("PowerPoint.Application")
+presentation = app.Presentations.Open(FileName=u'C:\\Users\\PRANJUL\\Desktop\\FYP\\controlling_ppt\\test.pptx', ReadOnly=1)
+presentation.SlideShowSettings.Run()
 
 while(True):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: #creating socket with ipv4 and port
@@ -69,17 +72,19 @@ while(True):
                 if not datatemp:
                     break
                 elif finaldata=="forward":
-                    app = win32com.client.Dispatch("PowerPoint.Application")
-                    presentation = app.Presentations.Open(FileName=u'C:\\Users\\PRANJUL\\Desktop\\FYP\\controlling_ppt\\test.pptx', ReadOnly=1)
-                    presentation.SlideShowSettings.Run()
-                    presentation.SlideShowWindow.View.Next()
+                    # app = win32com.client.Dispatch("PowerPoint.Application")
+                    # presentation = app.Presentations.Open(FileName=u'C:\\Users\\PRANJUL\\Desktop\\FYP\\controlling_ppt\\test.pptx', ReadOnly=1)
+                    # presentation.SlideShowSettings.Run()
+                    # presentation.SlideShowWindow.View.Next()
+                    print("commented hai, hatane se pehle global lines ko upar le jaana mat bhoolna")
                 elif finaldata=="backward":
-                    app = win32com.client.Dispatch("PowerPoint.Application")
-                    presentation = app.Presentations.Open(FileName=u'C:\\Users\\PRANJUL\\Desktop\\FYP\\controlling_ppt\\test.pptx', ReadOnly=1)
-                    presentation.SlideShowSettings.Run()
-                    presentation.SlideShowWindow.View.Previous()
-                elif finaldata[0]=="predicting":
-                    predict(finaldata[1])
+                    # app = win32com.client.Dispatch("PowerPoint.Application")
+                    # presentation = app.Presentations.Open(FileName=u'C:\\Users\\PRANJUL\\Desktop\\FYP\\controlling_ppt\\test.pptx', ReadOnly=1)
+                    # presentation.SlideShowSettings.Run()
+                    # presentation.SlideShowWindow.View.Previous()
+                    print("commented hai, hatane se pehle global lines ko upar le jaana mat bhoolna")
+                elif finaldata[0]=="Predicting":
+                    predict(finaldata[1:-1])
                 elif finaldata[0]=="CaptureData":
                     print("here")
                     CaptureData(finaldata[1],finaldata[2:-1])
